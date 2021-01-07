b0009a358032a1df7cf075765e284e431d73ec4b
Paranjay-Sharma <76864845+Paranjay-Sharma@users.noreply.github.com>
2021-01-07 17:20:22 +0530

    Add files via upload

---
+++ ninja/sketch.js
@@ -0,0 +1,31 @@
+var sword;
+var fruit1;
+var fruit2;
+var fruit3;
+var fruit4;
+var dirt;
+
+function preload(){
+  
+
+  fruit1 = loadImage("fruit1.png");
+  fruit2 = loadImage("fruit2.png");
+  fruit3 = loadImage("fruit3.png");
+  fruit4 = loadImage("fruit4.png");
+  sword = loadImage("sword.png");
+  dirt = loadImage("alien.png"); 
+}
+
+
+function setup;{
+  
+
+}
+
+
+
+
+
+function draw(){
+
+}
